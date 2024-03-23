12915e3c281ab3a9c79ac95e2a74711045862fe7
root <root@LTER-SEMENOV.lan.lanit.ru>
2024-03-23 14:00:12 +0300

    feat(): add format option

--- src/handler.php
+++ src/handler.php
@@ -10,10 +10,13 @@ Generate diff
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: stylish]
 DOC;
+
     $args = \Docopt::handle($doc, ['version' => 'cli 1.0']);
 }
